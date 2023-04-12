17fe3f7bf2401e2a8d14eefd0d203ade8394db9e
Unkown496 <kroloeden@gmail.com>
2023-04-12 20:55:07 +0300

    first add

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 // Import all of Bootstrap's JS
 import "../scss/main.scss";
 import * as bootstrap from 'bootstrap';
+
+// let a = async () => {
+//     const res = await axiosConfig();
+
+//     if(res.data.task_status !== 'SUCCESS') return 
+//     setTimeout(async () => {
+//         return await a();
+//     }, 1000);
+// };
+// let timeId = setTimeout(async function tick() {
+//     let res = await a();
+
+//     if(res.data.task_status !== 'SUCCESS') {
+//        return timeId = setTimeout(tick, 1000);
+//     };
+
+//     return res;
+// }, 1000);
