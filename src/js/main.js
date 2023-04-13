f131bd8c58d7673a188dd1b1a0fdd909831f51dd
Unkown496 <kroloeden@gmail.com>
2023-04-13 04:28:06 +0300

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,6 @@
 // Import all of Bootstrap's JS
 import "../scss/main.scss";
-<<<<<<< main
-import * as bootstrap from 'bootstrap';
-=======
 import * as bootstrap from 'bootstrap';
 
 import "./pageSelector.js";
 import "./sidebar.js";
->>>>>>> local
